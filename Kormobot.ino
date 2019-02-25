9a82d6ee5f6fdcf15567729360e3f016c2c770ee
Mikhaylichenko, Denis <denis.mikhaylichenko@misys.com>
2019-02-25 18:58:08 +0300

    code cleanup added led pin 4 to indicate thrown food

--- Kormobot.ino
+++ Kormobot.ino
@@ -1,19 +1,24 @@
 #include <Servo.h>
 
+#define DEBUG true
+
 #define BTLED_PIN 13
+#define LED01_PIN 4
 #define SERVO_PIN 9
 #define IROBS_PIN 8
 #define PIRMS_PIN 3
 #define BUTTN_PIN 2
 
 //CONSTANTS
-const unsigned int SERIAL_SPEED = 9600;
-const unsigned int OPEN_DELAY = 700;
+const unsigned long MILLIS_DECOUNT = 5000;//1200000;
 const unsigned long LONG_WAIT = 5000; //1200000;
 const unsigned long SHORT_WAIT = 2000;
+
 const byte ANGLE_OPEN = 94;
 const byte ANGLE_CLOSED = 4;
-const unsigned long MILLIS_DECOUNT = 5000;//1200000;
+
+const unsigned int SERIAL_SPEED = 9600;
+const unsigned int OPEN_DELAY = 700;
 const unsigned int MAX_FOOD = 20;
 const unsigned int MAX_FOOD_RELAX = 4;
 const unsigned int LOTS_OF_FOOD = 3;
@@ -30,8 +35,13 @@ bool switchFeedMode = false;
 
 void setup()
 {
+        
+#ifdef DEBUG
     Serial.begin(SERIAL_SPEED);
+#endif 
+   
     pinMode(BTLED_PIN, OUTPUT);
+    pinMode(LED01_PIN, OUTPUT);
     pinMode(IROBS_PIN, INPUT);
     pinMode(PIRMS_PIN, INPUT);
     pinMode(BUTTN_PIN, INPUT);
@@ -41,7 +51,7 @@ void setup()
 
 void loop() 
 {
-    if(CheckManualFeedButton())
+    if(IsManualFeedButtonDown())
     {
         ThrowSomeFood(A_BIT_OF_FOOD, true);
         return;
@@ -52,14 +62,11 @@ void loop()
     if (IsOverFeed())
     {
         switchFeedMode = true;
-        for(int i = 0; i < 10; i++)
-        {
-            digitalWrite(BTLED_PIN, HIGH);
-            delay(50);
-            digitalWrite(BTLED_PIN, LOW);
-            delay(50);
-        }
+        BlinkLed(BTLED_PIN, 7, 50);
+
+#ifdef DEBUG
         Serial.println("long wait");
+#endif
         delay(LONG_WAIT);
         return;
     }
@@ -68,8 +75,12 @@ void loop()
     {
         isEatTime = !isEatTime;
         switchFeedMode = false;
+
+#ifdef DEBUG
         Serial.print("isEatTime ");
         Serial.println(isEatTime);
+#endif   
+
     }
 
     if (CheckIR())
@@ -114,10 +125,34 @@ void ComputeTimeFromLastCountDecrease()
         
         lastMilliseconds = currentMilliseconds;
     }
-    Serial.println(foodCounter);
+
+    IndicateFoodCount();
 }
 
-bool CheckManualFeedButton()
+void IndicateFoodCount()
+{
+    digitalWrite(LED01_PIN, LOW);
+
+    if ((foodCounter > LOTS_OF_FOOD) && (foodCounter < (MAX_FOOD / 2)))
+    {
+        BlinkLed(LED01_PIN, 1, 50);
+    }
+    else if (foodCounter >= (MAX_FOOD / 2))
+    {
+        BlinkLed(LED01_PIN, 3, 50);
+    }
+    else if (foodCounter >= MAX_FOOD)
+    {
+       digitalWrite(LED01_PIN, HIGH);
+    }
+
+#ifdef DEBUG
+    Serial.println(foodCounter);
+#endif 
+    
+}
+
+bool IsManualFeedButtonDown()
 {
     return (digitalRead(BUTTN_PIN) == HIGH);
 }
@@ -127,14 +162,12 @@ bool CheckIR()
     int val = digitalRead(IROBS_PIN); 
     if (val == LOW)
     {
-        for(int i = 0; i < 1; i++)
-        {
-            digitalWrite(BTLED_PIN, HIGH);
-            delay(50);
-            digitalWrite(BTLED_PIN, LOW);
-            delay(50);
-        }
+        BlinkLed(BTLED_PIN, 1, 50);
+
+#ifdef DEBUG
         Serial.println("Obstacle detected");
+#endif 
+
         return true;
     }
 
@@ -146,14 +179,12 @@ void CheckMotion()
     int val = digitalRead(PIRMS_PIN); 
     if (val != LOW)
     {
-        for(int i = 0; i < 3; i++)
-        {
-            digitalWrite(BTLED_PIN, HIGH);
-            delay(50);
-            digitalWrite(BTLED_PIN, LOW);
-            delay(50);
-        }
+        BlinkLed(BTLED_PIN, 3, 50);
+        
+#ifdef DEBUG
         Serial.println("Motion detected");
+#endif 
+      
         isMoveDetected = true;
     }
 }
@@ -165,25 +196,15 @@ void ThrowSomeFood(int times, bool manual)
 
     myservo.attach(SERVO_PIN);
 
-    for(int i = 0; i < 3; i++)
-    {
-        digitalWrite(BTLED_PIN, HIGH);
-        delay(100);
-        digitalWrite(BTLED_PIN, LOW);
-        delay(50);
-    }
-
     while (times > 0)
     {
-       // myservo.write(ANGLE_OPEN);
-        //Serial.println("open");
-
+        BlinkLed(BTLED_PIN, 1, 100);
+        //myservo.write(ANGLE_OPEN);
         delay(OPEN_DELAY);
 
-       // myservo.write(ANGLE_CLOSED);
-        //Serial.println("closed");
-
-        delay(500);
+        //myservo.write(ANGLE_CLOSED);
+        BlinkLed(BTLED_PIN, 1, 100);
+        delay(300);
 
         times--;
 
@@ -196,4 +217,15 @@ void ThrowSomeFood(int times, bool manual)
 
     myservo.detach();
     
+}
+
+void BlinkLed(byte pin, int times, int delayMs)
+{
+    for(int i = 0; i < times; i++)
+    {
+        digitalWrite(pin, HIGH);
+        delay(delayMs);
+        digitalWrite(pin, LOW);
+        delay(delayMs);
+    }
 }
